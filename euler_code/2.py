9277ddaacce29abfa756d81f827b8cf4803b8b59
Nigel Nicholas <nigelnicholas@Engkans-MacBook-Air.local>
2018-10-28 22:50:31 +0800

    Finished comments

--- euler_code/2.py
+++ euler_code/2.py
@@ -15,6 +15,9 @@ Student ID : 1155088791
 """
 
 
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#                                                           Class for Creating Object Graph
+
 class Graph(object):
 
     def __init__(self):
@@ -27,7 +30,15 @@ class Graph(object):
         self.current_path = []  # stack for tracking the visting edges 
         self.eulerian_path = [] # stack for storing the eulerian path
 
+#- - - - - - - - - - - - 
+#Function add_node
+#Input = character: string
+#Output = none
 
+#Create a new object Node if it hasn't existed yet
+#Add character into the list of characters that already exist
+#Insert the new node into into list of nodes in the graph and map the node object to a certain index
+#- - - - - - - - - - - - - - 
     def add_node(self, character):
         # if node with such character exists, return
         if character in self.character_list:
@@ -39,6 +50,16 @@ class Graph(object):
         self.position_in_list[character] = self.index
         self.index = self.index + 1
 
+#- - - - - - - - - - 
+#Function add_edge
+#Input = input1:string  ,  input2: string,      weight: string  
+#Output = __
+
+#Create a new edge that connects input1->input2 ( directed edge )
+#Store it in a list of edges in the graph
+#Update indegree and outdegree 
+#- - - - - - - - - - 
+
     def add_edge(self, char1, char2, weight):   
         u = self.vertex_list[self.position_in_list[char1]]    # u is node object
         v = self.vertex_list[self.position_in_list[char2]]    # v is node object
@@ -49,6 +70,16 @@ class Graph(object):
         v.indegree += 1
         u.outdegree += 1
 
+#- - - - - - - - - - - - - - - - 
+# Function find_start
+# Input: __
+# Output: obj: type Node
+
+# Traverse through all the vertex and find one with more outdegree than indegree, if not select any of nodes
+# Assumption : The graph always has eulerian path and therefore always gives valid requirement for Eulerian Path
+# 
+#- - - - - - - - - - - - - - - - 
+
     def find_start(self):
         # Finding the node that has >0 outdeg-indeg
         for vertex in self.vertex_list:
@@ -58,6 +89,19 @@ class Graph(object):
         # By the time you got here, then there are no vertex with out-in = 1
 
         return self.vertex_list[0]
+
+#- - - - - - - - - - - - - - - - -
+# Function euler
+# Input = start : type Node; 
+# Output = __ 
+
+# Function that fills in the eulerian path stack and moves around current_path
+# Algorithm :
+# Keep track of the current path , for every node visited , push it to the stack
+# If at some point you're stuck push the current path into the eulerian path stack
+
+#- - - - - - - - - - - - - - - - 
+
     
     def euler(self, start):
         # start is of type: Nodes
@@ -71,6 +115,16 @@ class Graph(object):
         self.current_path.pop()
         return
     
+#- - - - - - - - - - - - - - - - 
+# Function print_euler
+# Input : Graph Object of the caller
+# Output : Result:string
+
+# From the euler_path stack, reverse the stack then print accordingly
+# Take the first full string then keep appending last element of each node.string
+
+#- - - - - - - - - - - - - - - - 
+
     def print_euler(self):
         eulerpath = self.eulerian_path[-1].content
         self.eulerian_path.pop()
@@ -85,6 +139,8 @@ class Graph(object):
         return eulerpath
 
 
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#                                                       Class for Creating Node Object
 class Nodes(object):
 
     def __init__(self,alphabet):
@@ -94,6 +150,9 @@ class Nodes(object):
         self.content = alphabet
         self.edges = []
 
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#                                                       Class for Creating Edge Object
 class Edge(object):
     # u is the initial node
     # v is the target node
@@ -102,6 +161,9 @@ class Edge(object):
         self.v = node2
         self.weight = weight
 
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#                                                       Main Function to put the prompt in
 
 G = Graph()
 
