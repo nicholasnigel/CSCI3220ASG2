349af6bd7a18aaef1928c8b285c8d15004b1300d
Nigel Nicholas <nigelnicholas@Engkans-MacBook-Air.local>
2018-10-28 16:14:06 +0800

    declaration added

--- euler_code/2.py
+++ euler_code/2.py
@@ -1,5 +1,19 @@
 import sys
 
+""""
+CSCI3220 2018-19 First Term Assignment 2
+I declare that the assignment here submitted is original except for source
+material explicitly acknowledged, and that the same or closely related material
+has not been previously submitted for another course. I also acknowledge that I
+am aware of University policy and regulations on honesty in academic work, and
+of the disciplinary guidelines and procedures applicable to breaches of such
+policy and regulations, as contained in the following websites.
+University Guideline on Academic Honesty:
+http://www.cuhk.edu.hk/policy/academichonesty/
+Student Name: Nigel Nicholas
+Student ID : 1155088791
+"""
+
 
 class Graph(object):
 
